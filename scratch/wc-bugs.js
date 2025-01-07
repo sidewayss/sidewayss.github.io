0d1d5a60e9fcbcbcc5ce222a2dd8f91369cdee45
Sideways S. <sidewayss@users.noreply.github.com>
2025-01-06 19:34:15 -0600

    wc-bugs complete for now

--- scratch/wc-bugs.js
+++ scratch/wc-bugs.js
@@ -29,5 +29,35 @@ class MyElm2 extends BaseElm {
         super("t2");
     }
 }
+class MyElm3 extends BaseElm {
+    constructor() {
+        super("t3");
+    }
+}
+class MyElm4 extends BaseElm {
+    #bool;
+    constructor() {
+        super("t4");
+        this.#bool = false;
+        this._use  = this.shadowRoot.getElementById("use");
+        this.addEventListener("click", this.#click);
+    }
+    #click() {
+        this.#bool = !this.#bool;
+        this._use.setAttribute("href", "#click" + Number(this.#bool));
+    }
+}
+class MyElm5 extends MyElm4 {
+    constructor() {
+        super();
+        this.addEventListener("mousedown", this.#mouseDown);
+    }
+    #mouseDown() {
+        this._use.setAttribute("href", "#mousedown");
+    }
+}
 customElements.define("my-elm1", MyElm1);
 customElements.define("my-elm2", MyElm2);
+customElements.define("my-elm3", MyElm3);
+customElements.define("my-elm4", MyElm4);
+customElements.define("my-elm5", MyElm5);
